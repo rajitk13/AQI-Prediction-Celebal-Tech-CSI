876a3c5ffbf46da2a89988fc10c903658fe17c62
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-07-06 22:01:31 +0530

    Deployment Update #City_Wise

--- _deployment/app.py
+++ _deployment/app.py
@@ -4,14 +4,19 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 
-model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
+city="random_forest"
+model_file = city.lower() + "_model.pkl"
+model = pickle.load(open(model_file, 'rb'))
 
 def main():
     st.title("Air Quality Index Prediction")
-    st.write("Enter the feature values to predict the AQI:")
     
-    # Create input fields for features
+    city = st.selectbox("Select City", ["Ahmedabad", "Bengaluru", "Chandigarh", "Chennai",  "Delhi"])
+    model_file = city.lower() + "_model.pkl"
+    model = pickle.load(open(model_file, 'rb'))
+    
+    st.write("Enter the feature values to predict the AQI:")
     co = st.number_input("CO", value=0.5)
     no2 = st.number_input("NO2", value=20)
     o3 = st.number_input("O3", value=30)
@@ -20,14 +25,11 @@ def main():
     so2 = st.number_input("SO2", value=10)
     ts = st.number_input("TS", value=1600000000)
     
-    # Create a feature vector from input values
     features = np.array([[co, no2, o3, pm10, pm25, so2, ts]])
-    
-    # Make predictions using the loaded model
     prediction = model.predict(features)
     
-    # Display the predicted AQI
     st.write("Predicted AQI:", prediction[0])
+
 
 
 if __name__ == "__main__":
